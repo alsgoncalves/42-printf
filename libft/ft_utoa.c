7c32a622539c717b4b36955c736eaf4a29f3cd42
Ana Goncalves <alsgoncalves91@gmail.com>
2021-04-14 17:36:21 +0100

    fixed libft ft_utoa.c

--- libft/ft_utoa.c
+++ libft/ft_utoa.c
@@ -6,7 +6,7 @@
 /*   By: asobreir <asobreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 15:25:05 by asobreir          #+#    #+#             */
-/*   Updated: 2021/04/07 15:26:07 by asobreir         ###   ########.fr       */
+/*   Updated: 2021/04/14 17:35:09 by asobreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,37 +24,25 @@ int		n_length_u(unsigned int n)
 	return (length);
 }
 
-char	*ft_print_n_u(unsigned int n)
-{
-	char			*str;
-	int				negative;
-	unsigned int	index;
-
-	negative = 0;
-	if (n < 0)
-	{
-		negative += 1;
-		n = -n;
-	}
-	if (!(str = (char *)malloc(negative + n_length_u(n) + 1)))
-		return (NULL);
-	index = n_length_u(n) + negative;
-	str[index--] = '\0';
-	while (n >= 10)
-	{
-		str[index--] = (n % 10) + '0';
-		n /= 10;
-	}
-	if (n % 10 >= 0)
-		str[index--] = n + '0';
-	if (negative == 1)
-		str[index] = '-';
-	return (str);
-}
-
 char	*ft_utoa(unsigned int n)
 {
+	char			*str;
+	unsigned int	index;
+
 	if (n == 0)
 		return (ft_strdup("0"));
-	return (ft_print_n_u(n));
+	else 
+	{
+		if (!(str = (char *)malloc(n_length_u(n) + 1)))
+			return (NULL);
+		index = n_length_u(n);
+		str[index--] = '\0';
+		while (n >= 10)
+		{
+			str[index--] = (n % 10) + '0';
+			n /= 10;
+		}
+		str[index--] = n + '0';
+	}
+	return (str);
 }
